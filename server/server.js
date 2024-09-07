76d8cc48718b22fb5bea2023a0d292fc8bad34ad
ab ya <abstyle0701@gmail.com>
2024-09-07 15:14:26 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
+const contactRoutes = require('./routes/contact');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -12,20 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const contactRoutes = require('./routes/contact');
 app.use('/api/contact', contactRoutes);
 
-// Serve static files from the React app (for production)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  // Serve the React app for any unknown routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
-
-// MongoDB connection
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log('MongoDB connection error:', error));
